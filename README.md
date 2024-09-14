a8afa451fbe7af7ce9edb0605f9e6f813b849f7e
Miguel Aquino <miguelmendes203@gmail.com>
2024-09-14 01:22:07 -0300

    2.14 - Conhecendo os pontos de injeção e a anotação @ Autowired

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Anotações:
 
 2.13 - @Bean: Indica que o método anotado vai instanciar um objeto da classe que retorna no método. O nome do Bean é a assinatura do método.  
        @Configuration: Também um componente Spring, porém é usada para definir @Beans  
+
+2.14 - @Autowired: Anotação usada para definir o ponto de injeção. Pode ser usado em construtores, método Set, no próprio atributo.  
--- algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -3,20 +3,33 @@ package com.algaworks.algafood.service;
 
 import com.algaworks.algafood.modelo.Cliente;
 import com.algaworks.algafood.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+@Component
 public class AtivacaoClienteService {
 
+    @Autowired
     private Notificador notificador;
 
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-        System.out.println("Ativacao Cliente Service "+ notificador);
-    }
+//    @Autowired
+//    public AtivacaoClienteService(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
+
+//    public AtivacaoClienteService(String qualquer) {
+//
+//    }
+
+//    @Autowired
+//    public void setNotificador(Notificador notificador) {
+//        this.notificador = notificador;
+//    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
     }
+
 }
