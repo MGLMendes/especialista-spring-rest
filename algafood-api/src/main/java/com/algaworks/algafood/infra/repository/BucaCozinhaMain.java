1fa9aee6b6b6019c017e5581550ca7ce83f07233
Miguel Aquino <miguelmendes203@gmail.com>
2024-09-22 23:58:19 -0300

    3.10 - Buscando um objeto pelo id no banco de dados

--- algafood-api/src/main/java/com/algaworks/algafood/infra/repository/BucaCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infra/repository/BucaCozinhaMain.java
@@ -6,15 +6,16 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class ListaCozinhaMain {
+public class BucaCozinhaMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
                 .web(WebApplicationType.NONE).run(args);
 
 
         CadastroCozinha bean = applicationContext.getBean(CadastroCozinha.class);
-        for (Cozinha cozinha : bean.listaCozinha()) {
-            System.out.println(cozinha.getNome());
-        }
+
+        Cozinha buscar = bean.buscar(1L);
+        System.out.println(buscar.getNome());
+
     }
 }
--- algafood-api/src/main/java/com/algaworks/algafood/infra/repository/InclusaoCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infra/repository/InclusaoCozinhaMain.java
@@ -6,15 +6,20 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class CadastraCozinhaMain {
+public class InclusaoCozinhaMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
                 .web(WebApplicationType.NONE).run(args);
 
 
         CadastroCozinha bean = applicationContext.getBean(CadastroCozinha.class);
-        for (Cozinha cozinha : bean.listaCozinha()) {
-            System.out.println(cozinha.getNome());
-        }
+        Cozinha cozinha = new Cozinha();
+        cozinha.setNome("Brasileira");
+
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Japonesa");
+
+        bean.adicionar(cozinha);
+        bean.adicionar(cozinha1);
     }
 }
