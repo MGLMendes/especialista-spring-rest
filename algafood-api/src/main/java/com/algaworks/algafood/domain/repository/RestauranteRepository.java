069e33651965f7a3a3a08c04ef7ed467ec1f8d52
Miguel Aquino <miguelmendes203@gmail.com>
2024-09-23 14:13:35 -0300

    3.16 - Desafio: Lombok e repositório de restaurantes

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.domain.repository;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+
+import java.util.List;
+
+public interface RestauranteRepository {
+
+    List<Restaurante> listar();
+
+    Restaurante buscar(Long id);
+    Restaurante salvar(Restaurante Restaurante);
+    void remover(Restaurante Restaurante);
+}
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,2 +1,6 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+
+
+insert into restaurante (nome, taxa_frete) values ('XingIng', 25.0)
+insert into restaurante (nome, taxa_frete) values ('NongAwss', 15.0)
