6cd66903b57f830a8b2a60c4ed99732244be3f38
Miguel Aquino <miguelmendes203@gmail.com>
2024-10-17 10:24:42 -0300

    5.7 - Usando as keywords para definir criterios de query methods

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/TestController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/TestController.java
@@ -1,11 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CozinhaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -15,11 +18,28 @@ public class TestController {
 
     private final CozinhaRepository cozinhaRepository;
 
-    private final CozinhaService cozinhaService;
+    private final RestauranteRepository restauranteRepository;
 
     @GetMapping("/cozinhas-por-nome")
     public List<Cozinha> listar(@RequestParam String nome) {
-        return cozinhaRepository.findByNome(nome);
+        return cozinhaRepository.findByNomeContaining(nome);
+    }
+
+
+    @GetMapping("/restaurantes-por-taxa-frete")
+    public List<Restaurante> restaurantesPorTaxaFrete(
+            @RequestParam BigDecimal taxaInicial,
+            @RequestParam BigDecimal taxaFinal
+            ) {
+        return restauranteRepository.findByTaxaFreteBetween(taxaInicial, taxaFinal);
+    }
+
+    @GetMapping("/restaurantes-e-cozinha-por-id")
+    public List<Restaurante> restaurantesECozinhasPorId(
+            @RequestParam String nome,
+            @RequestParam Long cozinhaId
+    ) {
+        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
     }
 
 
