e805a73d3f4f090aec4598e45a026f64d4f0b398
Miguel Aquino <miguelmendes203@gmail.com>
2024-12-30 16:36:50 -0300

    11.10 - Implementando a conversão de entidade para DTO e exclusao do Metodo Patch em RestauranteController

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.controller;
 
 
+import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.RestauranteModel;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.ValidacaoException;
@@ -25,6 +26,7 @@ import javax.validation.ValidationException;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -36,89 +38,50 @@ public class RestauranteController {
     private final SmartValidator smartValidator;
 
     @GetMapping
-    public ResponseEntity<List<Restaurante>> listar() {
-        return ResponseEntity.ok(restauranteService.listar());
+    public ResponseEntity<List<RestauranteModel>> listar() {
+        return ResponseEntity.ok(toCollectionList(restauranteService.listar()));
     }
 
     @GetMapping("/{restauranteId}")
     public ResponseEntity<RestauranteModel> buscar(@PathVariable Long restauranteId) {
-        Restaurante buscar = restauranteService.buscar(restauranteId);
-        RestauranteModel restauranteModel = null;
+        Restaurante restaurante = restauranteService.buscar(restauranteId);
+        RestauranteModel restauranteModel = toModel(restaurante);
         return ResponseEntity.ok(restauranteModel);
 
     }
 
     @PostMapping
-    public ResponseEntity<?> salvar(
+    public ResponseEntity<RestauranteModel> salvar(
             @RequestBody @Valid Restaurante restaurante) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(restauranteService.salvar(restaurante));
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                toModel(restauranteService.salvar(restaurante)));
 
     }
 
     @PutMapping("/{restauranteId}")
-    public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody @Valid Restaurante restaurante) {
-        return ResponseEntity.ok(restauranteService.atualizar(restauranteId, restaurante));
+    public ResponseEntity<RestauranteModel> atualizar(@PathVariable Long restauranteId, @RequestBody @Valid Restaurante restaurante) {
+        return ResponseEntity.ok(
+                toModel(restauranteService.atualizar(restauranteId, restaurante)));
 
     }
 
-    @PatchMapping("/{restauranteId}")
-    public ResponseEntity<?> atualizarParcial(@PathVariable Long restauranteId,
-                                              @RequestBody Map<String, Object> campos,
-                                              HttpServletRequest request) {
-        try {
-            Restaurante restauranteSalvo = restauranteService.buscar(restauranteId);
 
-            merge(campos, restauranteSalvo, request);
-            validate(restauranteSalvo, "restaurante");
+    private RestauranteModel toModel(Restaurante restaurante) {
+        CozinhaModel cozinhaModel = new CozinhaModel();
+        cozinhaModel.setId(restaurante.getCozinha().getId());
+        cozinhaModel.setNome(restaurante.getCozinha().getNome());
 
-            return ResponseEntity.ok(atualizar(restauranteId, restauranteSalvo));
-        } catch (EntidadeNaoEncontradaException e) {
-            return ResponseEntity.notFound().build();
-        }
+        RestauranteModel restauranteModel = new RestauranteModel();
+        restauranteModel.setId(restaurante.getId());
+        restauranteModel.setNome(restaurante.getNome());
+        restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());
+        restauranteModel.setCozinha(cozinhaModel);
+        return restauranteModel;
     }
 
-    private void validate(Restaurante restaurante, String objectName) {
-        BeanPropertyBindingResult beanPropertyBindingResult = new BeanPropertyBindingResult(
-                restaurante, objectName
-        );
-
-        smartValidator.validate(
-                restaurante,beanPropertyBindingResult
-        );
-
-        if (beanPropertyBindingResult.hasErrors()) {
-            throw new ValidacaoException(beanPropertyBindingResult);
-        }
-    }
-
-    private void merge(Map<String, Object> campos, Restaurante restauranteDestino,
-                       HttpServletRequest request) {
-
-        ServletServerHttpRequest servletServerHttpRequest = new ServletServerHttpRequest(request);
-
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-
-            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
-            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
-
-            Restaurante restauranteOrigem = objectMapper.convertValue(campos, Restaurante.class);
-
-
-            campos.forEach(
-                    (nomePropriedade, valorPropriedade) -> {
-                        Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-                        if (field != null) {
-                            field.setAccessible(true);
-
-                            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-                            ReflectionUtils.setField(field, restauranteDestino, novoValor);
-                        }
-                    }
-            );
-        }catch (IllegalArgumentException e) {
-            Throwable rootCause = ExceptionUtils.getRootCause(e);
-            throw new HttpMessageNotReadableException(e.getMessage(), rootCause, servletServerHttpRequest);
-        }
+    private List<RestauranteModel> toCollectionList(List<Restaurante> restaurantes) {
+        return restaurantes.stream().map(
+                this::toModel  // restaurante -> toModel(restaurante)
+        ).collect(Collectors.toList());
     }
 }
