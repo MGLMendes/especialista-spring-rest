72dc1e5ba00a2a05b53ff6e22a287a33b5bfc5f4
Miguel Aquino <miguelmendes203@gmail.com>
2024-12-29 23:01:26 -0300

    8.21 - Tratando a exception InvalidFormatException na desserialização

--- algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,6 +3,9 @@ package com.algaworks.algafood.api.exceptionhandler;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +15,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if (rootCause instanceof InvalidFormatException) {
+            return handleInvalidFormatException((InvalidFormatException) rootCause,
+                    headers,
+                    status,
+                    request);
+        }
 
         Problem problem = createProblemBuilder(
                 status,
@@ -36,7 +49,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,
-                                             WebRequest request) {
+                                                                  WebRequest request) {
 
         HttpStatus status = HttpStatus.NOT_FOUND;
 
@@ -119,11 +132,37 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
             HttpStatus status,
             ProblemType problemType,
             String detail
-    ){
+    ) {
         return Problem.builder()
                 .status(status.value())
                 .type(problemType.getUri())
                 .title(problemType.getTitle())
                 .detail(detail);
+    }
+
+
+    private ResponseEntity<Object> handleInvalidFormatException(
+            InvalidFormatException ex, HttpHeaders headers, HttpStatus status, WebRequest request
+    ) {
+
+        String path = ex.getPath().stream()
+                .map(JsonMappingException.Reference::getFieldName)  // ou .map(ref -> ref.getFieldName())
+                .collect(Collectors.joining("."));
+
+        String detail = String.format(
+                "A propriedade '%s' recebeu o valor '%s', " +
+                        "que é de um tipo inválido. " +
+                        "Corrija e informe um valor compatível com o tipo '%s'",
+                path, ex.getValue(), ex.getTargetType().getSimpleName()
+        );
+
+
+        Problem problem = createProblemBuilder(
+                status,
+                ProblemType.MENSAGEM_INCOMPREENSIVEL,
+                detail).build();
+
+        return handleExceptionInternal(ex, problem, headers, status, request);
+
     }
 }
