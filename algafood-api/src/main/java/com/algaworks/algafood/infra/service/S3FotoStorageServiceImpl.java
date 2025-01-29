1aec161eac77a7640281d2beb28af35a5e555329
Miguel Aquino <miguelmendes203@gmail.com>
2025-01-28 21:05:55 -0300

    14.21 - Adicionando o SDK Java da Amazon S3 no projeto e criando classe do serviço de storage

---
+++ algafood-api/src/main/java/com/algaworks/algafood/infra/service/S3FotoStorageServiceImpl.java
@@ -0,0 +1,44 @@
+package com.algaworks.algafood.infra.service;
+
+import com.algaworks.algafood.core.storage.StorageProperties;
+import com.algaworks.algafood.domain.exception.StorageException;
+import com.algaworks.algafood.domain.service.FotoStorageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class S3FotoStorageServiceImpl implements FotoStorageService {
+
+    private final StorageProperties storageProperties;
+
+
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+        return null;
+    }
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+
+    }
+
+    @Override
+    public void remover(String nomeArquivo) {
+
+    }
+
+    @Override
+    public void verificarCompatibilidadeMediaType(MediaType mediaType, List<MediaType> acceptMediaTypes) throws HttpMediaTypeNotAcceptableException {
+
+    }
+}
