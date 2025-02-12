e66a22c6c96106f1d24d45e9175baa9e92c6905f
Miguel Aquino <miguelmendes203@gmail.com>
2025-02-12 18:47:21 -0300

    16.8 - Implementando um formulário de cadastro e fazendo requisição POST com JavaScript

--- client-js/client.js
+++ client-js/client.js
@@ -25,5 +25,42 @@ function consultar() {
     });
   }
   
+
+function cadastrar() {
+
+    var formaPagamento = JSON.stringify({
+        "descricao": $("#campo-descricao").val()
+    });
+
+    console.log(formaPagamento)
+
+    $.ajax({
+        url: "http://localhost:8080/formas-pagamento",
+        type: "post", 
+
+        data: formaPagamento,
+        contentType: "application/json",
+
+        success: function() {
+            alert("Forma Pagamento Cadastrada");
+            consultar();
+
+        },
+
+        error: function(error) {
+            if (error.status == 400) {
+                var problem = JSON.parse(error.responseText)
+                alert(problem.userMessage)
+
+            } else {
+                alert("Erro ao cadastrar forma de pagamento!")
+            }
+
+        }
+    })
+    
+}
+
+$("#btn-consultar").click(consultar);
   
-  $("#btn-consultar").click(consultar);
+$("#btn-cadastrar").click(cadastrar);
