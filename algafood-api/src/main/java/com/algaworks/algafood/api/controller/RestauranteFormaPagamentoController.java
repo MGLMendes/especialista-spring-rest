6e00ac3a3ad5a23a29ae16fa4b7b9db972af3597
Miguel Aquino <miguelmendes203@gmail.com>
2025-02-17 19:46:09 -0300

    18.32 - Desafio: descrevendo documentação de endpoints de associação de restaurantes com formas de pagamento

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -2,11 +2,8 @@ package com.algaworks.algafood.api.controller;
 
 
 import com.algaworks.algafood.api.assembler.FormaPagamentoDTOAssembler;
-import com.algaworks.algafood.api.assembler.RestauranteDTOAssembler;
-import com.algaworks.algafood.api.disassembler.RestauranteInputDisassembler;
 import com.algaworks.algafood.api.model.dto.FormaPagamentoDTO;
-import com.algaworks.algafood.api.model.dto.RestauranteDTO;
-import com.algaworks.algafood.api.model.input.RestauranteInput;
+import com.algaworks.algafood.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.RestauranteService;
 import lombok.RequiredArgsConstructor;
@@ -14,13 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/formas-pagamento")
 @RequiredArgsConstructor
-public class RestauranteFormaPagamentoController {
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 
     private final RestauranteService restauranteService;
 
@@ -34,21 +30,17 @@ public class RestauranteFormaPagamentoController {
         return ResponseEntity.ok(formaPagamentoDTOAssembler.toCollectionList(restaurante.getFormasPagamento()));
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{formaPagamentoId}")
-    public ResponseEntity<List<FormaPagamentoDTO>> vicular(@PathVariable Long restauranteId,
+    public void vincular(@PathVariable Long restauranteId,
                                                                @PathVariable Long formaPagamentoId) {
-
         restauranteService.vincularFormaPagamento(restauranteId, formaPagamentoId);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{formaPagamentoId}")
-    public ResponseEntity<List<FormaPagamentoDTO>> desvincular(@PathVariable Long restauranteId,
+    public void desvincular(@PathVariable Long restauranteId,
                                                                @PathVariable Long formaPagamentoId) {
-
         restauranteService.desvincularFormaPagamento(restauranteId, formaPagamentoId);
-
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
