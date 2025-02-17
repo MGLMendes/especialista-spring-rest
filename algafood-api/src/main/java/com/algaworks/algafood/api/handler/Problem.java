acba199d74cd46920effc6b6d8f623d0425f1623
Miguel Aquino <miguelmendes203@gmail.com>
2025-02-17 16:22:17 -0300

    18.14 - Descrevendo o modelo de representação de problemavv

--- algafood-api/src/main/java/com/algaworks/algafood/api/handler/Problem.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/handler/Problem.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood.api.handler;
 
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,25 +11,63 @@ import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.List;
 
+@ApiModel(
+        value = "Problema"
+)
 @Getter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Problem {
+
+    @ApiModelProperty(
+            example = "400"
+    )
     private Integer status;
+
+    @ApiModelProperty(
+            example = "https://algafood.com.br/dados-invalidos"
+    )
     private String type;
+
+    @ApiModelProperty(
+            example = "Dados Inválidos!Dados Inválidos!"
+    )
     private String title;
+
+    @ApiModelProperty(
+            example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente."
+    )
+
     private String detail;
-
-
+    @ApiModelProperty(
+            example = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente."
+    )
     private String userMessage;
+
+    @ApiModelProperty(
+            example = "2025-02-17T16:17:19.1241395-03:00"
+    )
     private OffsetDateTime timestamp;
+
+    @ApiModelProperty(
+            value = "Objetos que geraram o erro"
+    )
     private List<Object> objects;
 
 
+    @ApiModel(
+            value = "Objeto Problema"
+    )
     @Getter
     @Builder
     public static class Object {
+        @ApiModelProperty(
+                value = "taxaFrete"
+        )
         private String name;
+        @ApiModelProperty(
+                value = "Taxa de frete do restaurante é obrigatória"
+        )
         private String userMessage;
     }
 }
--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.core.openapi;
 
+import com.algaworks.algafood.api.handler.Problem;
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -31,6 +33,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket apiDocket() {
+        var typeResolver = new TypeResolver();
+
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
@@ -41,6 +46,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 .globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
                 .globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
                 .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
+                .additionalModels(typeResolver.resolve(Problem.class))
                 .apiInfo(apiInfo())
                 .tags(new Tag("Cidades", "Gerencia as cidades"));
     }
