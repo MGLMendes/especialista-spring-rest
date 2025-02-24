29b5cde1dcc5549f288d8bdcdddbf76208d1f389
Miguel Aquino <miguelmendes203@gmail.com>
2025-02-24 10:07:58 -0300

    19.13 - Corrigindo link de coleção de recurso de responsáveis por restaurante

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -7,10 +7,14 @@ import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.RestauranteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/restaurantes/{restauranteId}/responsaveis", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -20,20 +24,22 @@ public class RestauranteUsuarioResponsavelController implements RestauranteUsuar
     private final RestauranteService restauranteService;
 
     private final UsuarioDTOAssembler usuarioDTOAssembler;
-    
+
     @GetMapping
     public ResponseEntity<CollectionModel<UsuarioDTO>> listar(@PathVariable Long restauranteId) {
         Restaurante restaurante = restauranteService.buscar(restauranteId);
-        
-        return ResponseEntity.ok(usuarioDTOAssembler.toCollectionModel(restaurante.getResponsaveis()));
+
+        return ResponseEntity.ok(usuarioDTOAssembler.toCollectionModel(restaurante.getResponsaveis())
+                .removeLinks()
+                .add(linkTo(methodOn(this.getClass()).listar(restauranteId)).withSelfRel()));
     }
-    
+
     @DeleteMapping("/{usuarioId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void desassociar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
         restauranteService.desvincularResponsavel(restauranteId, usuarioId);
     }
-    
+
     @PutMapping("/{usuarioId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void associar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
