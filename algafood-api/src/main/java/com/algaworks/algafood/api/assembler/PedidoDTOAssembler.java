582ef5effcdc5fca910e84b256a07916367786fe
Miguel Aquino <miguelmendes203@gmail.com>
2025-02-24 11:18:34 -0300

    19.18 - Adicionando links com template variables

--- algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoDTOAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/assembler/PedidoDTOAssembler.java
@@ -5,6 +5,10 @@ import com.algaworks.algafood.api.model.dto.PedidoDTO;
 import com.algaworks.algafood.domain.model.Pedido;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
@@ -26,32 +30,45 @@ public class PedidoDTOAssembler extends RepresentationModelAssemblerSupport<Pedi
 
     @Override
     public PedidoDTO toModel(Pedido pedido) {
-        PedidoDTO PedidoDTO = createModelWithId(pedido.getCodigo(), pedido);
-        modelMapper.map(pedido, PedidoDTO);
+        PedidoDTO pedidoDTO = createModelWithId(pedido.getCodigo(), pedido);
+        modelMapper.map(pedido, pedidoDTO);
 
-        PedidoDTO.add(linkTo(PedidoController.class).withRel("pedidos"));
+        TemplateVariables pageVariables = new TemplateVariables(
+                new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM)
+        );
 
-        PedidoDTO.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
+        String pedidosUrl = linkTo(PedidoController.class).toUri().toString();
+
+        pedidoDTO.add(new Link(UriTemplate.of(
+                pedidosUrl,
+                pageVariables
+        ), "pedidos"));
+
+//        pedidoDTO.add(linkTo(PedidoController.class).withRel("pedidos"));
+
+        pedidoDTO.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
                 .buscar(pedido.getRestaurante().getId())).withSelfRel());
 
-        PedidoDTO.getCliente().add(linkTo(methodOn(UsuarioController.class)
+        pedidoDTO.getCliente().add(linkTo(methodOn(UsuarioController.class)
                 .buscar(pedido.getCliente().getId())).withSelfRel());
 
         // Passamos null no segundo argumento, porque é indiferente para a
         // construção da URL do recurso de forma de pagamento
-        PedidoDTO.getFormaPagamento().add(linkTo(methodOn(FormaPagamentoController.class)
+        pedidoDTO.getFormaPagamento().add(linkTo(methodOn(FormaPagamentoController.class)
                 .buscar(pedido.getFormaPagamento().getId(), null)).withSelfRel());
 
-        PedidoDTO.getEnderecoEntrega().getCidade().add(linkTo(methodOn(CidadeController.class)
+        pedidoDTO.getEnderecoEntrega().getCidade().add(linkTo(methodOn(CidadeController.class)
                 .buscar(pedido.getEnderecoEntrega().getCidade().getId())).withSelfRel());
 
-        PedidoDTO.getItens().forEach(item -> {
+        pedidoDTO.getItens().forEach(item -> {
             item.add(linkTo(methodOn(RestauranteProdutosController.class)
-                    .buscar(PedidoDTO.getRestaurante().getId(), item.getProdutoId()))
+                    .buscar(pedidoDTO.getRestaurante().getId(), item.getProdutoId()))
                     .withRel("produto"));
         });
 
-        return PedidoDTO;
+        return pedidoDTO;
     }
 
 }
