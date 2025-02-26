a741c40ba42243f8bba5ab2fafedca6a48835423
Miguel Aquino <miguelmendes203@gmail.com>
2025-02-26 19:08:05 -0300

    19.30 - Desafio: adicionando links de associação de restaurantes com responsáveis

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -8,14 +8,14 @@ import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.RestauranteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
+
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(value = "/restaurantes/{restauranteId}/responsaveis", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -32,21 +32,31 @@ public class RestauranteUsuarioResponsavelController implements RestauranteUsuar
     public ResponseEntity<CollectionModel<UsuarioDTO>> listar(@PathVariable Long restauranteId) {
         Restaurante restaurante = restauranteService.buscar(restauranteId);
 
-        return ResponseEntity.ok(usuarioDTOAssembler.toCollectionModel(restaurante.getResponsaveis())
+        ResponseEntity<CollectionModel<UsuarioDTO>> collectionModelResponseEntity = ResponseEntity.ok(usuarioDTOAssembler.toCollectionModel(restaurante.getResponsaveis())
                 .removeLinks()
-                .add(algaLinks.linkToResponsaveisRestaurante(restauranteId)));
+                .add(algaLinks.linkToResponsaveisRestaurante(restauranteId))
+                .add(algaLinks.linkToRestauranteResponsavelAssociacao(restauranteId, "associar")));
+
+        Objects.requireNonNull(collectionModelResponseEntity.getBody()).getContent().forEach(usuarioModel -> {
+            usuarioModel.add(algaLinks.linkToRestauranteResponsavelDesassociacao(
+                    restauranteId, usuarioModel.getId(), "desassociar"));
+        });
+
+        return collectionModelResponseEntity;
     }
 
     @DeleteMapping("/{usuarioId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void desassociar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
+    public ResponseEntity<Void> desassociar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
         restauranteService.desvincularResponsavel(restauranteId, usuarioId);
+        return ResponseEntity.noContent().build();
     }
 
     @Override
     @PutMapping("/{usuarioId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void associar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
+    public ResponseEntity<Void> associar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
         restauranteService.vincularResponsavel(restauranteId, usuarioId);
+        return ResponseEntity.noContent().build();
     }
 }
